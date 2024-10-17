8c5bd2ecfd373dd94fab45f3078e528a0bc8e753
Douglasgls <douglaspaz955@gmail.com>
2024-10-17 16:33:56 -0300

    Corrige erro no refresh_JWT

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,14 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from fast_zero.routers.auth import router as auth
 from fast_zero.routers.todos import router as todos
 from fast_zero.routers.users import router as users
+
 
 app = FastAPI()
 
 app.include_router(users)
 app.include_router(auth)
 app.include_router(todos)
+
+app.middleware('http')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get('/')
